573e1acebf31f8146537a24674f6aa79697b10d7
Apurva <test@Apurva.local>
2026-05-16 19:59:52 +0530

    Add welcome modal with first-visit auto-show and persistent help button

--- src/components/layout/AppShell.jsx
+++ src/components/layout/AppShell.jsx
@@ -1,15 +1,31 @@
+import { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Sidebar } from './Sidebar'
 import { BottomNav } from './BottomNav'
+import { WelcomeModal } from '../ui/WelcomeModal'
 
 export function AppShell() {
+  const [helpOpen, setHelpOpen] = useState(false)
+
+  useEffect(() => {
+    if (!localStorage.getItem('taal_welcomed')) {
+      setHelpOpen(true)
+    }
+  }, [])
+
+  function handleClose() {
+    localStorage.setItem('taal_welcomed', '1')
+    setHelpOpen(false)
+  }
+
   return (
     <div className="flex min-h-screen bg-bg">
-      <Sidebar />
+      <Sidebar onHelp={() => setHelpOpen(true)} />
       <main className="flex-1 min-w-0 pb-20 md:pb-0">
         <Outlet />
       </main>
       <BottomNav />
+      <WelcomeModal open={helpOpen} onClose={handleClose} />
     </div>
   )
 }
